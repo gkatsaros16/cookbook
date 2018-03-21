74d729b26e39880794f8550cafd71bcb05ea62b1
George Katsaros <gkatsaros@harmonicdrive.net>
2018-03-21 09:01:01 -0400

    updating project and pushing to gh

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,17 +7,19 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'Simply a test', 'https://get.pxhere.com/photo/heart-pattern-red-circle-brand-font-illustration-design-shape-129402.jpg')
+    new Recipe('A Test Recipe', 'Simply a test', 'https://get.pxhere.com/photo/heart-pattern-red-circle-brand-font-illustration-design-shape-129402.jpg'),
+    new Recipe('My Second Recipe', 'This is not a test', 'https://get.pxhere.com/photo/heart-pattern-red-circle-brand-font-illustration-design-shape-129402.jpg'),
   ];
-
-  check() {
-    console.log(this.recipes[0].name)
-  }
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe)
+  }
 }
---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,5 @@
+//get set shortcut
+export class Ingredient {
+    constructor(public name: string, public amount: number) {
+    }
+}
